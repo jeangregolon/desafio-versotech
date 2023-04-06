77aca0787df755af01eb21e55ebd22ea721ac067
Jean Gregolon <jean.gregolon@gmail.com>
2023-04-06 15:09:44 -0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import PokemonList from './components/pokemonList';
+import PokemonDetail from './components/pokemonDetail';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<PokemonList/>} />
+        <Route path="/pokemon/:name" element={<PokemonDetail/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/pokemonDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PokemonDetail = () => {
+
+  return (
+    <div>
+        <h1>detail</h1>
+    </div>
+  );
+};
+
+export default PokemonDetail;
